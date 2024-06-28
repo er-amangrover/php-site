064297faf7d72dd4b50a16068897d57ef1217231
Aman Grover <aman.grover@nagarro.com>
2024-06-28 20:35:25 +0530

    test commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <div id="navigation">
+<ul>
+<li><a href="page1.php">Page One</a></li>
+<li><a href="page2.php">Page Two</a></li>
+</ul>
+</div>
+    </body>
+</html>
